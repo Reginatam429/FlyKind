3e29bb39f58883e22fb25f79f94e2f5bff3c0931
Regina <reginatam429@gmail.com>
2019-07-20 18:43:53 -0700

    Add new app

---
+++ mytest/lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlyKind',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: MyHomePage(),
+
+        );
+
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: new Image.asset('assets/icon.png', fit: BoxFit.contain,
+            height: 45,
+          )
+        ),
+        body: Center(child: SwipeList()));
+  }
+}
+
+class SwipeList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListItemWidget();
+  }
+}
+
+class ListItemWidget extends State<SwipeList> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+              },
+              child: Container(
+                width: 350,
+                height: 250,
+                decoration: new BoxDecoration(
+
+                  borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                  image: DecorationImage(
+                    image: new AssetImage(
+                        'assets/rainforest.jpg'),
+                    fit: BoxFit.fill,
+
+                  ),
+                ),
+                child: Text("RAIN FOREST",
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40)
+                ),
+                alignment: Alignment.center,
+
+              ),
+            ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                print('Card tapped.');
+
+              },
+              child: Container(
+                width: 350,
+                height: 250,
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                  image: DecorationImage(
+                    image: new AssetImage(
+                        'assets/undersea.jpg'),
+                    fit: BoxFit.fill,
+
+                  ),
+                ),
+                child: Text("UNDER WATER",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40)
+                ),
+                alignment: Alignment.center,
+
+              ),
+            ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                print('Card tapped.');
+              },
+              child: Container(
+                width: 350,
+                height: 250,
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                  image: DecorationImage(
+                    image: new AssetImage(
+                        'assets/sunsetcity.jpg'),
+                    fit: BoxFit.fill,
+
+                  ),
+                ),
+                child: Text("CITY NIGHT",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40)
+                ),
+                alignment: Alignment.center,
+
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
+
+
+//Rainforest Second Screen
+class SecondScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rain Forest',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Rain Forest'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/rain.gif"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Text("Just Breathe",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40)
+          ),
+          alignment: Alignment.center,
+
+        ),
+        ),
+      ),
+    );
+  }
+  }
+}
